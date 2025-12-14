080058a2f77c3e70d09f8fc2090cb2b29bbfe080
Angga Pratama <angga147a.com>
2025-12-14 15:24:35 +0700

    revisi-1

---
+++ android/app/src/main/java/app/lovable/d796a823c0de403bbb8d00c0295c2be8/MainActivity.java
@@ -0,0 +1,5 @@
+package app.lovable.d796a823c0de403bbb8d00c0295c2be8;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: 'app.lovable.d796a823c0de403bbb8d00c0295c2be8',
   appName: 'EduGame Kids',
   webDir: 'dist',
-  server: {
-    url: 'https://d796a823-c0de-403b-bb8d-00c0295c2be8.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  //server: {
+    //url: 'https://d796a823-c0de-403b-bb8d-00c0295c2be8.lovableproject.com?forceHideBadge=true',
+    //cleartext: true
+  //}
 };
 
 export default config;
