17de0dc4dbe4f029cf1d6b4c451ece864929a71c
Angga Pratama <angga147a.com>
2025-12-14 16:45:18 +0700

    revisi-3

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { GameProvider } from "@/contexts/GameContext";
 import SplashScreen from "@/components/SplashScreen";
 import Index from "./pages/Index";
@@ -25,7 +25,7 @@ const App = () => {
           {showSplash && <SplashScreen onFinish={() => setShowSplash(false)} />}
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/game/reading" element={<WordArrangeGame />} />
@@ -34,7 +34,7 @@ const App = () => {
               <Route path="/game/sentence" element={<SentenceArrangeGame />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </TooltipProvider>
       </GameProvider>
     </QueryClientProvider>
